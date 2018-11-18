cdf6f834c9d38c8276b4fc903e331cec9846105c
Yiorgos Joseph Makridakis <yjm30@itl251.student.eecs.qmul.ac.uk>
2018-11-18 16:41:11 +0000

    find value -> trans.tx_hash -> vin.tx_hash -> vin.vout -> vout.vout -> vout.value

--- time_analysis.py
+++ time_analysis.py
@@ -7,34 +7,33 @@ class timeAnalysis(MRJob):
 
     #Create a bar plot showing the number of transactions which occurred every month between the start and end of the dataset. What do you notice about the overall trend in the utilisation of bitcoin?Note: As the dataset spans multiple years and you are aggregating together all transactions in the same month, make sure to include the year in your analysis.
 
-    tx_vin_vout = {}
+    tx_vout = {}
 
     def steps(MRJob):
-        return [ MRStep(mapper_init=self.join_tx_vin,
+        return [MRStep(mapper_init=self.init_dict), 
+                MRStep(mapper_init=self.join_tx_vin,
                         mapper=self.map_to_month_amount,
                         reducer=self.count_tx_amount),
                 MRStep( reducer=self.split_month)
                 ]
 
-    def join_tx_vout_vin(self):
-        with open("transactions.csv") as f:
+    
+    def join_tx_vin_vout(self, _, line):
+        arr = line.split(",")
+        #vout.csv has format hash, value, n(vout from vin), publicKey
+        
+
+    def join_tx_vin(self, _, line):
+        arr = line.split(",")
+        #vin.csv has format txid, tx_hash, vout
+        self.tx_vout[arr[1]].append(arr[2])
+
+    def init_dict(self):
+        with open("input/transactions.csv") as f:
            for line in f:
                 arr = line.split(",")
-
-
-
-    def mapper(self, _, line):
-        arr = line.split(",")
-
-        try:
-            tx_hash = arr[0]
-            date    = datetime.datetime.fromtimestamp(arr[2])
-            
-
-        except:
-            self.increment_counter('custom','bad_lines', 1)
-
-
+                #line format = tx_hash, blockhash, time, tx_in_count, tx_out_count
+                self.tx_vout[arr[0]] = [(datetime.datetime.fromtimestamp(arr[2]).strftime("%Y-%m"))]
 
 if __name__ == '__main__':
         timeAnalysis.run()
